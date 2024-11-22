a6d915b8583b97a786e84ef3e185b8ffce6ab8bf
Amira <amirakhaled2027@gmail.com>
2024-11-22 18:47:06 +0200

    changing the port from 5173 to 5174

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,13 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:3000',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     }
-  //   }
-  // }
+  server: {
+    port: 5174, // Specify the desired port here
+  },
+  
 })
